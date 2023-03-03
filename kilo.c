9bb3536ff66511e1eaf103264dc307f25637320a
RuiChenZhai <54008162+Malones314@users.noreply.github.com>
2023-03-03 19:23:13 +0800

    kilo.c step7

--- kilo.c
+++ kilo.c
@@ -1,15 +1,29 @@
 #include<unistd.h>
 #include<termios.h>
+#include<stdlib.h>
 
-void enableRawMode() {				//可以通过以下方式设置terminal的属性： 
-						//1.使用tcgetattr()将当前属性读入结构
-  struct termios raw;				//2.手动修改结构
-						//3.将修改后的结构传递给tcsetattr()以写入新的
-  tcgetattr( STDIN_FILENO, &raw);		//terminal属性退出
+struct termios orig_termios;
 
-  raw.c_lflag &= ~(ECHO);			//关闭ECHO
+void disableRawMode(){    //得到terminal的副本，配合atexit()函数在程序结束时还原terminal
+  tcgetattr( STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
+//可以通过以下方式设置terminal的属性：
+//1.使用tcgetattr()将当前属性读入结构
+//2.手动修改结构
+//3.将修改后的结构传递给tcsetattr()以写入新的terminal属性
+void enableRawMode() {				       
+  tcgetattr( STDIN_FILEND, &orig_termios);
+  atexit( disableRawMode);  //atexit 是线程安全的：从多个线程调用该函数不会引发数据竞争。
+                            //用它来注册 disableRawMode() 函数，以便在程序退出时自动调用
+  struct termios raw = orig_termios;	        
+  tcgetattr( STDIN_FILENO, &raw);	 
+
+  raw.c_lflag &= ~(ECHO | ICANON);			//关闭ECHO、ICANON,c_lflag：本地模式
+                                        //ICANON：标准模式，关闭后逐字节读取，而非逐行读取
   tcsetattr( STDIN_FILENO, TCSAFLUSH, &raw);
+    //STDIN_FILENO：stdin的文件编号，TCSAFLUSH：改变在所有写入STDIN_FILENO的输出都被传输后生效，所有已接受但未读入的输入都在改变发生前丢弃
+    //即终端不显示输入的字符
 }
 
 int main(){
